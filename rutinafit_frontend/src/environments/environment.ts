3788ba6d1cacb29891d0fadeb82f18eb9d5c07e5
raul <loaizapinoraul@gmail.com>
2026-05-13 09:56:02 +0200

    apiUrl fix

--- rutinafit_frontend/src/environments/environment.ts
+++ rutinafit_frontend/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // ==============================
 
 export const environment = {
-  apiUrl: 'https://rutinafit.onrender.com',
+  apiUrl: 'https://rutinafit-back.onrender.com',
   appName: 'RutinaFit',
   version: '1.0.0',
 };
