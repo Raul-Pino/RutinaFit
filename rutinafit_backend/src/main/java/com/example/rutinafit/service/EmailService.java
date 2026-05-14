36a960d5eeca31c2eb43c15db22ec8cc81c3b938
raul <loaizapinoraul@gmail.com>
2026-05-14 12:04:41 +0200

    Modificar sendEmail

--- rutinafit_backend/src/main/java/com/example/rutinafit/service/EmailService.java
+++ rutinafit_backend/src/main/java/com/example/rutinafit/service/EmailService.java
@@ -21,6 +21,7 @@ public class EmailService {
         try {
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            helper.setFrom("RutinaFit <rutinafit.app@gmail.com>");
             helper.setTo(to);
             helper.setSubject(subject);
             helper.setText(html, true);
